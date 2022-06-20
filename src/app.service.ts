7411ff7b084ba8798df34e05f3236f1b0de2fc75
jobum97 <brian654321@naver.com>
2022-06-20 12:33:35 +0900

    msg change

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!!!!!!';
+    return 'Bumsoo\'s Server homepages';
   }
 }
